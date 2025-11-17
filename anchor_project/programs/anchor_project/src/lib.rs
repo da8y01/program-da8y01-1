1657297eaa82ef60211683e6356933a541d45de7
Daniel Benitez <danielbenitezr@protonmail.com>
2025-11-17 00:45:17 +0000

    anchor init

---
+++ anchor_project/programs/anchor_project/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6asFXwC8aFqnwtyVARHkqg66K5MvigPUPPtS6hHXfT6n");
+
+#[program]
+pub mod anchor_project {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
