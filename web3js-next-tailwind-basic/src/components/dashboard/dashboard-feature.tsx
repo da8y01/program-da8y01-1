c461fece1f9acdfff2d0a447a0de1f8ec1afcb08
Daniel Benitez <danielbenitezr@protonmail.com>
2025-11-18 22:44:42 -0500

    Adds AuctionsLayout component.

--- web3js-next-tailwind-basic/src/components/dashboard/dashboard-feature.tsx
+++ web3js-next-tailwind-basic/src/components/dashboard/dashboard-feature.tsx
@@ -11,7 +11,7 @@ const links: { label: string; href: string }[] = [
 export function DashboardFeature() {
   return (
     <div>
-      <AppHero title="gm" subtitle="Say hi to your new Solana app." />
+      <AppHero title="Auctions on-chain" subtitle="These are on-chain auctions." />
       <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div className="space-y-2">
           <p>Here are some helpful links to get you started.</p>
